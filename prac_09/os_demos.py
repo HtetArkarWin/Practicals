4e1425b99cac3b1797fa290e72d9286983b39877
HtetArkarWin <HtetArkarWin@my.jcu.edu.au>
2022-05-21 20:13:10 +0630

    Rename

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -19,7 +19,10 @@ def main():
     # Make a new directory
     # The next time you run this, it will crash if the directory exists
     # TODO: Use exception handling to avoid the crash (just pass)
-    os.mkdir('temp')
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        print('temp directory already exist.')
 
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
@@ -35,7 +38,7 @@ def main():
         # os.rename(filename, new_name)
 
         # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
+        shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -54,7 +57,11 @@ def demo_walk():
         print("(Current working directory is: {})".format(os.getcwd()))
 
         # TODO: add a loop to rename the files
+        for filename in filenames:
+            new_name=os.path.join(directory_name, get_fixed_filename(filename))
+            filename=os.path.join(directory_name, filename)
+            print("Renaming {} to {}".format(filename,new_name))
+            os.rename(filename,new_name)
 
-
-main()
-# demo_walk()
+#main()
+demo_walk()
