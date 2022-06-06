aad2b5d97cb717adf63a5b7153b560e6942437c2
HtetArkarWin <HtetArkarWin@my.jcu.edu.au>
2022-06-06 19:49:22 +0630

    add random score checking

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,11 +2,16 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+
+
 def main():
 
     score = float(input("Enter score: "))
-    print(evaluate_score(score))
+    print("{} mark is {}".format(score,evaluate_score(score)))
 
+    score = random.randint(0, 100)
+    print("{} mark is {}".format(score,evaluate_score(score)))
 
 def evaluate_score(score):
     result=""
