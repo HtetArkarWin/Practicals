4566df15b930339c86d4d47f60ee9e986e79e503
HtetArkarWin <HtetArkarWin@my.jcu.edu.au>
2022-06-06 19:21:50 +0630

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,24 @@
 MIN_LENGTH = 2
+def main():
+
+    password = get_password()
+
+    print_asterisk(password)
 
 
-password = input("> ")
-while len(password)<MIN_LENGTH:
-    print("Password must be at least {} characters".format(MIN_LENGTH))
+def print_asterisk(password):
+    stars = ""
+    for n in range(len(password)):
+        stars = stars + "*"
+    print(stars)
+
+
+def get_password():
     password = input("> ")
+    while len(password) < MIN_LENGTH:
+        print("Password must be at least {} characters".format(MIN_LENGTH))
+        password = input("> ")
+    return password
 
-stars = ""
-for n in range (len(password)):
-    stars = stars + "*"
-print(stars)
+
+main()
