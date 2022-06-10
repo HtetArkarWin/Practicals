d6e505b880401b791602222682abaacfd60c4ed8
HtetArkarWin <HtetArkarWin@my.jcu.edu.au>
2022-06-11 00:53:20 +0800

    guitars' testing

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,12 @@
+from prac_06.guitar import Guitar
+
+guitar1 = Guitar("Gibson L-5 CES", 1924)
+guitar2 = Guitar("Another Guitar", 2015)
+
+print("{} get_age() - Expected 98. Got {}".format(guitar1.name, guitar1.get_age()))
+print("{} get_age() - Expected 7. Got {}".format(guitar2.name, guitar2.get_age()))
+
+print("{} is_vintage() - Expected True. Got {}".format(guitar1.name, guitar1.is_vintage()))
+print("{} is_vintage() - Expected False. Got {}".format(guitar2.name, guitar2.is_vintage()))
+
+
